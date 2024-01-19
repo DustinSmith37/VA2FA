6f39a45b5e2da5afb002fa67c53cca920d087642
DustinSmith37 <firesire@outlook.com>
2024-01-19 00:20:51 -0600

    APA File Setup and Debugging
    Divided up training data into necessary subfolders and began re-writing code in order to process everything as needed, following changes to libraries between tutorial version and this version. Still a WIP

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # VA2FA
 # Reqs: Python 3.11.7 and the following libraries:
-os, pandas, numpy, librosa, sklearn (pip installed as scikit-learn), keras, and tensorflow 
+os, pandas, numpy, librosa, sklearn (pip installed as scikit-learn), keras, tensorflow, and resampy
 (use pip to install these libraries' subrequirements. Comprehensive list to follow later
--- audioProcessing.py
+++ audioProcessing.py
@@ -9,47 +9,69 @@ import librosa as librosa
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from keras.src.utils.np_utils import to_categorical
 
-def wavToTrainingData(file):
-    
-
-    #list the files
-    filelist = os.listdir('30_speakers_train') 
-    #read them into pandas
-    train_df = pd.DataFrame(filelist)
+def pullDefaultUsersData(filePath: str = "./DefaultUsers"):
+    #grab files from directory, later will need to pull from somewhere else
+    #print(filePath)
+    fileList = os.listdir(filePath+"/Train")
+    #print(fileList) 
+    #load into pandas data frame data structure
+    trainDefaultUsers = pd.DataFrame(fileList)
     # Renaming the column name to file
-    train_df = train_df.rename(columns={0:'file'})
-    # Code in case we have to drop the '.DS_Store' and reset the index
-    train_df[train_df['file']=='.DS_Store']
-    train_df.drop(16, inplace=True)
-    train_df = train_df.sample(frac=1).reset_index(drop=True)
+    trainDefaultUsers = trainDefaultUsers.rename(columns={0:'file'})
     # We create an empty list where we will append all the speakers ids for each row of our dataframe by slicing the file name since we know the id is the first number before the hash
     speaker = []
-    for i in range(0, len(df)):
-        speaker.append(df['file'][i].split('-')[0])
+    for i in range(len(trainDefaultUsers)):
+        speaker.append(trainDefaultUsers['file'][i].split('-')[0])
     # We now assign the speaker to a new column 
-    train_df['speaker'] = speaker
+    trainDefaultUsers['speaker'] = speaker
+    print(trainDefaultUsers.head())
 
-    def extract_features(files):
-        # Sets the name to be the path to where the file is in my computer
-        file_name = os.path.join(os.path.abspath('30_speakers_train')+'/'+str(files.file))
-        # Loads the audio file as a floating point time series and assigns the default sample rate
-        # Sample rate is set to 22050 by default
-        X, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
-        # Generate Mel-frequency cepstral coefficients (MFCCs) from a time series 
-        mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
-        # Generates a Short-time Fourier transform (STFT) to use in the chroma_stft
-        stft = np.abs(librosa.stft(X))
-        # Computes a chromagram from a waveform or power spectrogram.
-        chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
-        # Computes a mel-scaled spectrogram.
-        mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
-        # Computes spectral contrast
-        contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
-        # Computes the tonal centroid features (tonnetz)
-        tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
-        return mfccs, chroma, mel, contrast, tonnetz
+    #repeat for validating and testing data
+    fileList = os.listdir(filePath+"/Validate")
+    validDefaultUsers = pd.DataFrame(fileList)
+    validDefaultUsers = validDefaultUsers.rename(columns={0:'file'})
+    speaker = []
+    for i in range(len(validDefaultUsers)):
+        speaker.append(validDefaultUsers['file'][i].split('-')[0])
+    validDefaultUsers['speaker'] = speaker
+    print(validDefaultUsers.head())
 
-    train_features = train_df.apply(extract_features, axis=1)
+    fileList = os.listdir(filePath+"/Test")
+    testDefaultUsers = pd.DataFrame(fileList)
+    testDefaultUsers = testDefaultUsers.rename(columns={0:'file'})
+    speaker = []
+    for i in range(len(testDefaultUsers)):
+        speaker.append(testDefaultUsers['file'][i].split('-')[0])
+    testDefaultUsers['speaker'] = speaker
+    print(testDefaultUsers.head())
+
+    return trainDefaultUsers,validDefaultUsers,testDefaultUsers
+
+def extractFeaturesTrain(dataframe):
+    # Sets the name to be the path to where the individual file is
+    fileName = "./DefaultUsers/Train"+'/'+str(dataframe.file)
+    print(fileName)
+    # Loads the audio file as a floating point time series and assigns the default sample rate
+    # Sample rate is set to 22050 by default
+    X, sample_rate = librosa.load(fileName, res_type='kaiser_fast')
+    # Generate Mel-frequency cepstral coefficients (MFCCs) from a time series 
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
+    # Generates a Short-time Fourier transform (STFT) to use in the chroma_stft
+    stft = np.abs(librosa.stft(X))
+    # Computes a chromagram from a waveform or power spectrogram.
+    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+    # Computes a mel-scaled spectrogram. CURRENTLY ERRORS OUT DIRECTLY BELOW HERE!!!! MELSPECTROGRAM WORKS DIFFERENT NOW
+    mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+    # Computes spectral contrast
+    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+    # Computes the tonal centroid features (tonnetz)
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
+    return mfccs, chroma, mel, contrast, tonnetz
+
+def main():
+    print("Yippie!")
+    train,valid,test = pullDefaultUsersData()
+    trainFeaturesProcessed = train.apply(extractFeaturesTrain, axis=1)
 
 
     features_train = []
@@ -77,8 +99,6 @@ def wavToTrainingData(file):
     X_test = ss.transform(X_test)
 
     train_features = train_df.apply(extract_features, axis=1)
-def main():
-    print("Yippie!")
-    pass
+
 if __name__ == "__main__":
     main()
