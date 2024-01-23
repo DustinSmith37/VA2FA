bbf1d99ec2f26b1eb463bc7c842ce327cf5a824b
GMiculek <80844562+GMiculek@users.noreply.github.com>
2024-01-22 22:12:39 -0600

    Add files via upload

---
+++ audio record and upload to db (one process)/upload.php
@@ -0,0 +1,14 @@
+<?php
+
+
+include "db_conn.php"; 
+    		
+$new_audio_name = uniqid("audio-", true). '.'.'.wav'; //new audio name, gives timestamp
+$audio_upload_path = 'uploads/'.$new_audio_name;
+move_uploaded_file($_FILES['file']['tmp_name'], $audio_upload_path); //moves to uploads folder
+
+$sql = "INSERT INTO audios(audio_url) 
+                   VALUES('$new_audio_name')"; //adds new file to data base
+mysqli_query($conn, $sql); //actually adds new file to data base 
+
+?>
